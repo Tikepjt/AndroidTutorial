ef11a10313c860716323e2852ed37fb09283c6b0
Unknown <Tikepjt@gmail.com>
2018-01-09 20:41:09 +0900

    3_4Activity Update
    예제 버그 다잡음

--- part2-3/src/main/java/com/tistory/tikepjt/part2_3/Lab3_4Activity.java
+++ part2-3/src/main/java/com/tistory/tikepjt/part2_3/Lab3_4Activity.java
@@ -1,11 +1,9 @@
 package com.tistory.tikepjt.part2_3;
 
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.TextView;
 
 public class Lab3_4Activity extends AppCompatActivity {
 
@@ -16,18 +14,18 @@ public class Lab3_4Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab3_4);
 
-        TextView textView=(TextView)findViewById(R.id.fontView);
-        Typeface typeface=Typeface.createFromAsset(getAssets(), "xmas.ttf");
-        textView.setTypeface(typeface);
+//        TextView textView=(TextView)findViewById(R.id.fontView);
+//        Typeface typeface=Typeface.createFromAsset(getAssets(), "xmas.ttf");
+//        textView.setTypeface(typeface);
 
         checkBox=(CheckBox)findViewById(R.id.checkbox);
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    checkBox.setText("is Checked");
+                    checkBox.setText(getString(R.string.is_Checked));
                 } else {
-                    checkBox.setText("is unChecked");
+                    checkBox.setText(getString(R.string.is_unChecked));
                 }
             }
         });
