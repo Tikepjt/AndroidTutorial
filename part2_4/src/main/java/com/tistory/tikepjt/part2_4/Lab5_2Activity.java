2685ace63cc7f8f4164cb7479b0797f71e84faa0
Unknown <Tikepjt@gmail.com>
2018-01-14 17:45:35 +0900

    LAb5_2Activity.java file modified_01

--- part2_4/src/main/java/com/tistory/tikepjt/part2_4/Lab5_2Activity.java
+++ part2_4/src/main/java/com/tistory/tikepjt/part2_4/Lab5_2Activity.java
@@ -18,7 +18,7 @@ import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class Lab5_2Activity extends AppCompatActivity {
+public class Lab5_2Activity extends AppCompatActivity implements View.OnClickListener{
 
     Button alertBtn;
     Button listBtn;
@@ -36,11 +36,11 @@ public class Lab5_2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab5_2);
 
-        alertBtn = (Button) findViewById (R.id.btn_alert);
-        listBtn = (Button) findViewById (R.id.btn_list);
-        progressBtn = (Button) findViewById (R.id.btn_progress);
-        timeBtn = (Button) findViewById (R.id.btn_time);
-        customDialogBtn = (Button) findViewById (R.id.btn_custom);
+        alertBtn = findViewById (R.id.btn_alert);
+        listBtn = findViewById (R.id.btn_list);
+        progressBtn = findViewById (R.id.btn_progress);
+        timeBtn = findViewById (R.id.btn_time);
+        customDialogBtn = findViewById (R.id.btn_custom);
 
         alertBtn.setOnClickListener(this);
         listBtn.setOnClickListener(this);
@@ -55,7 +55,7 @@ public class Lab5_2Activity extends AppCompatActivity {
         toast.show();
     }
 
-        DialogInterface.OnCancelListener dialogListner = new DialogInterface.OnClickListener(){
+        DialogInterface.OnCancelListener dialogListener = new DialogInterface.OnClickListener(){
         @Override
         public void onClick(DialogInterface dialog, int which){
             if (dialog==customDialog && which==DialogInterface.BUTTON_POSITIVE){
@@ -74,7 +74,7 @@ public class Lab5_2Activity extends AppCompatActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Alert");
             builder.setMessage("Really shut down?");
-            builder.setPositiveButton("OK", dialogListner);
+            builder.setPositiveButton("OK", dialogListener);
             builder.setNegativeButton("NO", null);
 
             alertDialog = builder.create();
@@ -83,10 +83,10 @@ public class Lab5_2Activity extends AppCompatActivity {
         }else if (v == listBtn){
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("alarm sound");
-            builder.setSingleChoiceItems(R.array.dialog_array, 0, dialogListner);
+            builder.setSingleChoiceItems(R.array.dialog_array, 0, dialogListener);
 
             builder.setPositiveButton("allow", null);
-            builder.setPositiveButton("cancel", null);
+            builder.setNegativeButton("cancel", null);
             listDialog = builder.create();
             listDialog.show();
 
@@ -109,21 +109,21 @@ public class Lab5_2Activity extends AppCompatActivity {
             DatePickerDialog dateDialog = new DatePickerDialog(this,
                 new DatePickerDialog.OnDateSetListener() {
                     @Override
-                    public void onDateSet(DatePicker view, int year, int month){
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
                         showToast(year + ":" + (monthOfYear + 1) + ":" + dayOfMonth);
                     }
                 }, year, month, day);
                 dateDialog.show();
         }else if (v = timeBtn){
             Calendar c = Calendar.getInstance();
-            final int hour = c.get(Calendar.HOUR_OF_DAY);
+            int hour = c.get(Calendar.HOUR_OF_DAY);
             int minute = c.get(Calendar.MINUTE);
 
             TimePickerDialog timeDialog = new TimePickerDialog(this,
                     new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute){
-                        showToast(hourOfDay + "minute");
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                            showToast(hourOfDay + ":" + minute);
                     }
             }, hour, minute, false);
             timeDialog.show();
@@ -132,7 +132,7 @@ public class Lab5_2Activity extends AppCompatActivity {
             LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
             builder.setView(view);
 
-            builder.setPositiveButton("allow", dialogListner);
+            builder.setPositiveButton("allow", dialogListener);
             builder.setNegativeButton("cancel", null);
 
             customDialog = builder.create();
