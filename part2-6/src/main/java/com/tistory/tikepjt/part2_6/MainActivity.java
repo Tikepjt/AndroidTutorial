764c04378ce99de50a3b41c7ededa0125b8c2293
Unknown <Tikepjt@gmail.com>
2018-01-15 23:55:20 +0900

    Event 처리 완료

--- part2-6/src/main/java/com/tistory/tikepjt/part2_6/MainActivity.java
+++ part2-6/src/main/java/com/tistory/tikepjt/part2_6/MainActivity.java
@@ -2,22 +2,48 @@ package com.tistory.tikepjt.part2_6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-implements View.OnClickListener, CompoundButton.OnCheckedChangeListener{
+implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    TextView bellTextView;
+    TextView labelTextView;
+    CheckBox repeatCheckView;
+    CheckBox vibrateCheckView;
+    Switch switchView;
+
+    float initX;
+    long initTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        bellTextView.setOnClickListener(this);
-        labellTextView.setOnClickListener(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        repeatCheckView.setOnClickedChangeedListener(this);
-        vibrateCheckView.setOnClickedChangeListener(this);
-        switchView.setOnClickedChangeListener(this);
+        bellTextView=findViewById(R.id.bell_name);
+        labelTextView= findViewById(R.id.label);
+        repeatCheckView=findViewById(R.id.repeatCheck);
+        vibrateCheckView=findViewById(R.id.vibrate);
+        switchView=findViewById(R.id.onOff);
+
+        bellTextView.setOnClickListener(this);
+        labelTextView.setOnClickListener(this);
+
+        repeatCheckView.setOnCheckedChangeListener(this);
+        vibrateCheckView.setOnCheckedChangeListener(this);
+        switchView.setOnCheckedChangeListener(this);
     }
     private void showToast(String message){
+        Toast toast=Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
     @Override
     public void onClick(View v) {
@@ -28,12 +54,42 @@ implements View.OnClickListener, CompoundButton.OnCheckedChangeListener{
         }
     }
     @Override
-    public void onCheckedChangeed(CompoundButton buttonView, boolean isChecked){
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
         if (buttonView==repeatCheckView){
-            showToast("repeat checkbox is " +isChecked);{
+            showToast("repeat checkbox is " +isChecked);
         }else if (buttonView==vibrateCheckView){
-            showToast("ivbrate checkbox is " +isChecked);{
+            showToast("vibrate checkbox is " +isChecked);
         }else if (buttonView==switchView){
-            showToast("switch is " + isChecked);}
+            showToast("switch is " + isChecked);
     }
 }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                initX = event.getRawX();
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                float diffX = initX - event.getRawX();
+                if (diffX > 30) {
+                    showToast("pushed the screen to the left.");
+                } else if (diffX < -30) {
+                    showToast("pushed the screen to the right.");
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onKeyDown(int keyCode, KeyEvent event) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (System.currentTimeMillis() - initTime > 3000) {
+                    showToast("Press again to exit.");
+                    initTime = System.currentTimeMillis();
+                } else {
+                    finish();
+                }
+                return true;
+            }
+            return super.onKeyDown(keyCode, event);
+        }
+    }
