327bf4fdb7082e977a94c7be728f4ad99b4e0334
Unknown <Tikepjt@gmail.com>
2018-01-24 21:45:57 +0900

    액티비티 이동중 종료됨

--- RedCard/app/src/main/java/com/example/earth_hyuppc/redcard/MainActivity.java
+++ RedCard/app/src/main/java/com/example/earth_hyuppc/redcard/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void OutBtnClicked(View v) {
-        Intent intent = new Intent(this, RedCardActivity.class);
-        startActivity(intent);
-    }
+//    EditText editText = (EditText)findViewById(R.id.editText);
+//    final String name = editText.getText().toString();
 
-//    final EditText nameText = (EditText) findViewById(R.id.editText);
-//    Button OutButton = (Button) findViewById(R.id.button);
+    Intent intent = new Intent(this, RedCardActivity.class);
+
+    startActivity(intent);
+
     }
-//}
+}
--- RedCard/app/src/main/java/com/example/earth_hyuppc/redcard/RedCardActivity.java
+++ RedCard/app/src/main/java/com/example/earth_hyuppc/redcard/RedCardActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class RedCardActivity extends AppCompatActivity {
 
@@ -16,6 +17,9 @@ public class RedCardActivity extends AppCompatActivity {
     public void BackBtnClicked(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
     }
+
+    TextView textView=(TextView)findViewById(R.id.textView);
+    Bundle intent=getIntent().getExtras();
+
 }
