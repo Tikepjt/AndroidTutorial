a8b4004a857a4838e651ce813879f76df2893d0b
Unknown <Tikepjt@gmail.com>
2018-01-28 21:48:59 +0900

    DB 관련 클래스 작성

---
+++ part3-8/src/main/java/com/tistory/tikepjt/part3_8/DBHelper.java
@@ -0,0 +1,12 @@
+package com.tistory.tikepjt.part3_8;
+
+/**
+ * Created by tike_pjt on 1/28/18.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper {
+}
